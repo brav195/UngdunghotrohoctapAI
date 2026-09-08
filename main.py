65b66a26f5ad9072065e8a8cc5950b0d3b8eb4f8
brav195 <nguyenanh1952k5@gmail.com>
2026-09-08 11:12:02 +0700

    Doc API key tu Streamlit secrets

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ except ModuleNotFoundError:
 
 
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("GEMINI_API_KEY") or st.secrets.get("GEMINI_API_KEY")
 
 model = None
 if api_key and genai:
